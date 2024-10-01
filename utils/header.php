21a7dbdc76bb0e3132174b2e6dbb4ca6595c58a4
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-01 23:45:02 +0200

    format

--- utils/header.php
+++ utils/header.php
@@ -1,12 +1,12 @@
 <header>
-        <h1>Livre d'Or</h1>
-        <nav>
-            <a class='button' href="index.php">Accueil</a>
-            <?php if (isset($_SESSION['username'])) : ?>
-                <a class='button' href="utils/logout.php">Déconnexion</a>
-            <?php else : ?>
-                <a class='button' href="login.php">Connexion</a>
-            <?php endif; ?>
-            </ul>
-        </nav>
+    <h1>Livre d'Or</h1>
+    <nav>
+        <a class='button' href="index.php">Accueil</a>
+        <?php if (isset($_SESSION['username'])) : ?>
+            <a class='button' href="utils/logout.php">Déconnexion</a>
+        <?php else : ?>
+            <a class='button' href="login.php">Connexion</a>
+        <?php endif; ?>
+        </ul>
+    </nav>
 </header>
