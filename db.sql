58c2bc0e45f7eac52e957cca23920043f69762e1
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-06 19:07:22 +0200

    petits ajustements

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE `GuestBook` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicod
 
 USE `GuestBook`;
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
@@ -10,12 +10,12 @@ CREATE TABLE `users` (
     UNIQUE KEY `username` (`username`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-CREATE TABLE `comments` (
+CREATE TABLE IF NOT EXISTS `comments` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `user_id` int(11) NOT NULL,
     `content` varchar(255) NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp NOT NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `updated_at` timestamp DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
     INDEX `user_id` (`user_id`)
